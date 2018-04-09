759940d1127b070704de7e6745e227294ffcb3fa
Trey Hakanson <treyhakanson@gmail.com>
2018-04-08 21:28:31 -0400

    Corrected limiter styles, adding productive suggestions

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,26 @@
 const LAST_RESET = "LAST_RESET";
 const LIMITED_SITES = "LIMITED_SITES";
+const NUM_SUGGESTIONS = 9;
+const RESOURCES = [
+   "https://librivox.org/",
+   "https://waitbutwhy.com/",
+   "http://www.bartleby.com/",
+   "https://www.ted.com/",
+   "http://ed.ted.com/",
+   "http://video.mit.edu/",
+   "https://www.youtube.com/user/smithsonianchannel",
+   "https://www.youtube.com/user/TheRoyalInstitution",
+   "https://www.startalkradio.net/",
+   "http://www.theskepticsguide.org/podcast/sgu",
+   "https://www.udemy.com/",
+   "https://www.khanacademy.org/",
+   "http://ureddit.com/",
+   "https://ocw.mit.edu/index.htm",
+   "https://www.masterclass.com/",
+   "http://www.instructables.com/",
+   "https://www.codecademy.com/",
+   "https://www.freecodecamp.com/",
+   "https://www.codewars.com/",
+   "https://www.hackerrank.com/",
+   "https://www.duolingo.com/"
+];
--- src/splash-script.js
+++ src/splash-script.js
@@ -1,4 +1,18 @@
 (function(browser) {
+   let s = new Set();
+   let i = 0;
+   const wrapper = document.querySelector(".splash-body__suggestions");
+
+   while (i < NUM_SUGGESTIONS) {
+      const idx = Math.floor(Math.random() * RESOURCES.length);
+      if (!s.has(idx)) {
+         const suggestion = createSuggestion(RESOURCES[idx]);
+         wrapper.append(suggestion);
+         s.add(idx);
+         i++;
+      }
+   }
+
    function getTime(lastReset) {
       let now = new Date();
       let diff =
@@ -25,6 +39,14 @@
    function updateClock(lastReset) {
       let tm = getTime(lastReset);
       document.body.querySelector(".splash-body__clock").textContent = tm;
+   }
+
+   function createSuggestion(suggestionURL) {
+      let suggestion = document.createElement("a");
+      suggestion.className = "suggestion";
+      suggestion.href = suggestionURL;
+      suggestion.textContent = suggestionURL;
+      return suggestion;
    }
 
    browser.storage.local.get([LAST_RESET]).then(function(data) {
